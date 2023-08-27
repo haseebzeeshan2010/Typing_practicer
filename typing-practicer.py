e8e8543c2ce8222ab22878df799ce5ef9266b8dc
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-27 20:38:11 +0100

    Make program which takes words from random quotes

--- typing-practicer.py
+++ typing-practicer.py
@@ -13,37 +13,38 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def display_progress(text, index):
-    progress = text[:index] + '_' + text[index+1:]
-    print("\r" + progress, end='', flush=True)
+def display_quote(quote):
+    print("Type the following quote:")
+    print(quote)
 
 def main():
-    words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+    with open('quotes.txt', 'r') as file:
+        quotes = file.readlines()
     
     while True:
-        chosen_word = random.choice(words)
-        print("Type the following word:")
-        print(chosen_word)
+        chosen_quote = random.choice(quotes)
+        display_quote(chosen_quote)
         
         index = 0
         previous_correct = False
-        while index < len(chosen_word):
-            char = chosen_word[index]
-            display_progress(chosen_word, index)
+        user_input = ''
+        while index < len(chosen_quote):
+            char = chosen_quote[index]
+            if char == user_input:
+                display_quote(chosen_quote)
+                index += 1
+                user_input = ''
+                continue
+            
             user_input = getch()
-            
-            while user_input != char:
+            if user_input != char:
                 print("\rIncorrect! Try again.", end='', flush=True)
-                display_progress(chosen_word, index)
-                user_input = getch()
+                display_quote(chosen_quote)
                 previous_correct = False
-            
-            if not previous_correct and index < len(chosen_word) - 1:
-                print("\rCorrect! Proceed to the next letter:", char)
-            previous_correct = True
-            index += 1
+            else:
+                previous_correct = True
         
-        print("\nCongratulations! You typed the entire word correctly.")
+        print("\nCongratulations! You typed the entire quote correctly.")
 
 if __name__ == "__main__":
     main()
