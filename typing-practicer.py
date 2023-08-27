b33b6f73c89e855340fdd4933f99518df42563ac
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-27 21:04:41 +0100

    Simplify code

--- typing-practicer.py
+++ typing-practicer.py
@@ -14,55 +14,47 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def display_quote(quote, index, wpm, error_count):
+def display_quote(quote, index, wpm, errors):
     if index < len(quote):
-        highlighted_char = quote[index]
-        formatted_quote = quote[:index] + f"\033[1;32m{highlighted_char}\033[0m" + quote[index+1:]
+        formatted_quote = quote[:index] + f"\033[1;32m{quote[index]}\033[0m" + quote[index+1:]
     else:
         formatted_quote = quote
-    print("Type the following quote:")
-    print(formatted_quote)
-    print(f"Average WPM: {wpm:.2f} | Errors: {error_count}")
+    print(f"Type the following quote:\n{formatted_quote}\nAverage WPM: {wpm:.2f} | Errors: {errors}")
 
-def main():
-    with open('quotes.txt', 'r') as file:
-        quotes = file.readlines()
+with open('quotes.txt', 'r') as file:
+    quotes = [line.strip() for line in file.readlines()]
+
+while True:
+    chosen_quote = random.choice(quotes)
+    display_quote(chosen_quote, 0, 0, 0)
     
-    while True:
-        chosen_quote = random.choice(quotes).strip()
-        display_quote(chosen_quote, 0, 0, 0)
+    index, errors = 0, 0
+    user_input = ''
+    start_time = time.time()
+    while index < len(chosen_quote):
+        char = chosen_quote[index]
         
-        index = 0
-        previous_correct = False
-        user_input = ''
-        start_time = time.time()
-        error_count = 0
-        while index < len(chosen_quote):
-            char = chosen_quote[index]
-            
-            if char == user_input:
-                index += 1
-                user_input = ''
-                display_quote(chosen_quote, index, 0, error_count)
-                continue
-            
-            user_input = getch()
-            if user_input != char:
-                print("\rIncorrect! Try again.", end='', flush=True)
-                display_quote(chosen_quote, index, 0, error_count)
-                previous_correct = False
-                error_count += 1
-            else:
-                previous_correct = True
-                index += 1
+        if char == user_input:
+            index += 1
+            user_input = ''
+            display_quote(chosen_quote, index, 0, errors)
+            continue
+        
+        user_input = getch()
+        if user_input != char:
+            print("\rIncorrect! Try again.", end='', flush=True)
+            display_quote(chosen_quote, index, 0, errors)
+            errors += 1
+        else:
+            index += 1
+            if index == len(chosen_quote):
                 end_time = time.time()
                 time_taken = end_time - start_time
-                char_count = index  # Update character count for calculating WPM
+                char_count = len(chosen_quote)
                 wpm = (char_count / 5) / (time_taken / 60)
-                display_quote(chosen_quote, index, wpm, error_count)
-        
-        print("\nCongratulations! You typed the entire quote correctly.")
-        input("\nPress Enter to continue...")
-
-if __name__ == "__main__":
-    main()
+            else:
+                wpm = 0
+            display_quote(chosen_quote, index, wpm, errors)
+    
+    print("\nCongratulations! You typed the entire quote correctly.")
+    input("\nPress Enter to continue...")
