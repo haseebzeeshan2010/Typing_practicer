fd08e75f171e328296e08b22925dea53fb426527
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-27 20:43:35 +0100

    Highlight the letter that has to be written

--- typing-practicer.py
+++ typing-practicer.py
@@ -13,36 +13,44 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def display_quote(quote):
+def display_quote(quote, index):
+    if index < len(quote):
+        highlighted_char = quote[index]
+        formatted_quote = quote[:index] + f"\033[1;33m{highlighted_char}\033[0m" + quote[index+1:]
+    else:
+        formatted_quote = quote
     print("Type the following quote:")
-    print(quote)
+    print(formatted_quote)
 
 def main():
     with open('quotes.txt', 'r') as file:
         quotes = file.readlines()
     
     while True:
-        chosen_quote = random.choice(quotes)
-        display_quote(chosen_quote)
+        chosen_quote = random.choice(quotes).strip()
+        display_quote(chosen_quote, 0)
         
         index = 0
         previous_correct = False
         user_input = ''
         while index < len(chosen_quote):
             char = chosen_quote[index]
+            
             if char == user_input:
-                display_quote(chosen_quote)
                 index += 1
                 user_input = ''
+                display_quote(chosen_quote, index)
                 continue
             
             user_input = getch()
             if user_input != char:
                 print("\rIncorrect! Try again.", end='', flush=True)
-                display_quote(chosen_quote)
+                display_quote(chosen_quote, index)
                 previous_correct = False
             else:
                 previous_correct = True
+                index += 1
+                display_quote(chosen_quote, index)
         
         print("\nCongratulations! You typed the entire quote correctly.")
 
